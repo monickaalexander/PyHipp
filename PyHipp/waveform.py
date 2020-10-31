0a9cbf523b6d33355e9529660d38f42eefc483d6
monickaalexander <72338081+monickaalexander@users.noreply.github.com>
2020-11-01 02:49:14 +0800

    Update waveform.py

--- PyHipp/waveform.py
+++ PyHipp/waveform.py
@@ -50,6 +50,12 @@ class Waveform(DPT.DPObject):
         self.channel_filename = [os.path.basename(pwd)]  
         template_filename = os.path.join(DPT.levels.resolve_level('day', self.channel_filename[0]),'mountains', self.channel_filename[0], 'output', 'templates.hkl')
         
+        aname = DPT.levels.normpath(os.path.dirname(pwd))
+        self.array_dict = dict()
+        self.array_dict[aname] = 0
+        self.numSets = 1
+        self.current_plot_type = None
+
         templates = hkl.load(template_filename)
         
         self.data = [np.squeeze(templates)]
@@ -69,6 +75,10 @@ class Waveform(DPT.DPObject):
         # It is useful to store the information of the objects for panning through in the future
         DPT.DPObject.append(self, wf)  # append self.setidx and self.dirs
         self.data = self.data + wf.data
+        for ar in wf.array_dict:
+            self.array_dict[ar] = self.numSets
+        self.numSets += 1
+
         # .........................................
         # ..................code...................
         # .........................................
